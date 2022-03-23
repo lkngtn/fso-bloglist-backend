3ea172bac35871ee8f03c9ed411513650126d2e8
lkngtn <luke@lukeduncan.net>
2022-03-22 18:12:39 -0700

    Factor routes out of app.js

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 const config = require('./utils/config')
-const logger = require('./utils/logger')
+// const logger = require('./utils/logger')
 
 const express = require('express')
 const app = express()
 
 const cors = require('cors') // Not sure this is needed yet
 const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-const Blog = require('./models/blog')
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter )
 
 module.exports = app
