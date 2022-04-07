5bb62ed40ad4e1b7126e21689603a975803a3188
lkngtn <luke@lukeduncan.net>
2022-04-07 16:41:21 -0700

    Add middleware for logging and error handling

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const config = require('./utils/config')
-const logger = require('./utils/logger')
-
 const express = require('express')
-require('express-async-errors')
 const app = express()
-
-const cors = require('cors') // Not sure this is needed yet
+const cors = require('cors')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 logger.info('Connecting to mongoDB...')
 const mongoUrl = config.MONGODB_URI
@@ -16,7 +18,13 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter )
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
