81d29f96ec8fac0315d4d6c5d1a9d18d894957d0
lkngtn <luke@lukeduncan.net>
2022-04-08 11:39:58 -0700

    Add username and password validation

--- controllers/users.js
+++ controllers/users.js
@@ -12,6 +12,12 @@ usersRouter.post('/', async (request, response) => {
     })
   }
 
+  if (password.length < 3) {
+    return response.status(400).json({
+      error: 'password must be atleast 3 characters'
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
