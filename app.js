f4a77ed832204a69f41324bbd896955bfb0c653e
lkngtn <luke@lukeduncan.net>
2022-04-14 11:28:29 -0700

    fix testing route

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use('/api/blogs', blogsRouter )
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
-  app.use('api/testing', testingRouter)
+  app.use('/api/testing', testingRouter)
 }
 
 app.use(middleware.unknownEndpoint)
